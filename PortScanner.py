5048e1fa03233252df64d7f7e32e6102ad945537
mahdi <mahdi.robatipoor@gmail.com>
2016-11-27 02:10:41 +0330

    add function checkValidInput

--- PortScanner.py
+++ PortScanner.py
@@ -50,22 +50,39 @@ def scanPort(hostname,ports):
         thread = threading.Thread(target=checkOpenPort,args=(ip,p))
         thread.start()
 
-def main():
-    global timeOut
+def checkValidInput(host,port,time_out):
     mnp = 65535  # maximum number port
+    if host != None and port != None: # check not null option
+        if max(port) <= mnp and min(port) >= 1: # check exist in range port number
+            if time_out >=1 and time_out <= 30:
+                return True
+            else:
+                print('%s time value not correct'%(red))
+                return False
+        else:
+            print('%s port number value not correct'%(red))
+            return False
+    else:
+        print('%s host and port should be set'%(red))
+        return False
+
+
+
+def main():
+
+    global timeOut
     example = 'to example scan range ports 20 util 100 google => python %(prog)s -H google.com -p 20 100'
     parser = argparse.ArgumentParser(description=example)
     parser.add_argument('-H','--host',dest='host',type=str)
     parser.add_argument('-p','--port',dest='port',type=int,nargs='+')
     parser.add_argument('-t', '--timeout', dest='timeOut', type=int,default=5,required=False)
     parser.add_argument('-v','--version', action='version', version='%(prog)s 0.1')
-    args = parser.parse_args()
-    if args.host != None and args.port != None: # check not null option
-        if max(args.port) <= mnp and min(args.port) >= 1: # check exist in range port number
-            timeOut = args.timeOut
-            scanPort(args.host, args.port)
+    args = parser.parse_args('-H w3school.com -p 80 90 -t 8'.split())
+
+    if checkValidInput(args.host,args.port,args.timeOut):
+        timeOut = args.timeOut
+        scanPort(args.host, args.port)
     else:
-        print('%s Error !!!'%(red))
         parser.print_help()
         sys.exit(1)
 if __name__ == '__main__':
