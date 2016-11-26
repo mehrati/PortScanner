f83fc0fbbffecd2608ef8a88c61f4d4934e20940
mahdi <mahdi.robatipoor@gmail.com>
2016-11-26 18:46:46 +0330

    improve code

--- PortScanner.py
+++ PortScanner.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Author Mahdi Robatipoor 
+# Author Mahdi Robatipoor
 # mahdi.robatipoor@gmail.com
 
 import argparse,sys,socket,threading
@@ -27,11 +27,11 @@ def checkOpenPort(ip,port):
 
 def scanPort(hostname,ports):
     try:
-        ip = socket.gethostbyname(hostname)
+        ip = socket.gethostbyname(hostname)  # get IP address target
     except Exception as ex:
         print(ex)
         sys.exit(1)
-    if len(ports) > 1:
+    if len(ports) == 2:
         if ports[0] < ports[1]:
             max_range = ports[1]
             max_range += 1
@@ -40,8 +40,11 @@ def scanPort(hostname,ports):
             max_range = ports[0]
             max_range += 1
             range_port = range(ports[1], max_range)
-    else:
+    elif len(ports) == 1:
         range_port = ports
+    else:
+        print('Please Enter Correct Option')
+        sys.exit(1)
 
     for p in range_port:
         thread = threading.Thread(target=checkOpenPort,args=(ip,p))
@@ -49,17 +52,19 @@ def scanPort(hostname,ports):
 
 def main():
     global timeOut
+    mnp = 65535  # maximum number port
     example = 'to example scan range ports 20 util 100 google => python %(prog)s -H google.com -p 20 100'
     parser = argparse.ArgumentParser(description=example)
     parser.add_argument('-H','--host',dest='host',type=str)
-    parser.add_argument('-p','--port',dest='port',type=int,nargs='+',choices=range(1, 65535))
+    parser.add_argument('-p','--port',dest='port',type=int,nargs='+')
     parser.add_argument('-t', '--timeout', dest='timeOut', type=int,default=5,required=False)
     parser.add_argument('-v','--version', action='version', version='%(prog)s 0.1')
     args = parser.parse_args()
-    if args.host != None and args.port != None:
-        scanPort(args.host, args.port)
-        timeOut = args.timeOut
-        print(timeOut)
+    if args.host != None and args.port != None: # check not null option
+        if max(args.port) <= mnp and min(args.port) >= 1: # check exist in range port number
+            scanPort(args.host, args.port)
+            timeOut = args.timeOut
+            print(timeOut)
     else:
         print('Error !!!')
         parser.print_help()
